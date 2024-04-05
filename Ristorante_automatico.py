f29191ed8db597c0f7a6180e656befe3c324bff7
Mattia Simini <166040300+met21seeds@users.noreply.github.com>
2024-04-05 22:52:02 +0200

    primo versionamento
    Da rivedere ma miglioriamo

--- Ristorante_automatico.py
+++ Ristorante_automatico.py
@@ -1,79 +1,68 @@
-# liste di partenza + contatori vari
-basi = ["Pasta frolla","Pan di spagna","Biscotto"]
+# da rivedere variabili prezzo_client e ciclo , un po di confusione
+# liste 
+basi = ["pasta frolla","pan di spagna","biscotto"]
 prezzi_basi = [2,4,6]
 farciture = ["crema","panna","cioccolata"]
 prezzi_farc = [3,5,7]
 toppings = ["canditi","zucchero a velo","praline"]
-prezzi_toppings = [4,2,8]
-count = 0 # contatore per i tavoli
-#ricavi = 0 guadagno ristorante
+prezzi_top = [4,2,8]
+tavolo = 1
+ricavi = 0 #guadagno ristorante
 tot_torte = 0
-# Ciclo clienti
-while True: 
-    cliente = input ("Inserisci il nome :")
-    count += 1
-    tavolo = count + 1
-    print ("Ciao", cliente ," il tuo tavolo è il numero", tavolo)
-
+# Ciclo clienti   
+while True :
+    cliente = input ("Può dirmi il suo nome? \n ")
+    print ("Ciao", cliente ,"! il tuo tavolo è il numero", tavolo)
+    tavolo = + 1
     # Che ingredienti vuoi per preparare la torta
     print ("Ecco gli ingredienti per la base della torta: \n" ,basi)
     scelta_base = input ("Scegli un ingrediente per la base della tua torta \n")
-    # prezzo base:
-    if scelta_base == 0 :
-        prezzo_client = prezzi_basi[0] 
-    elif scelta_base == 1 :
-        prezzo_client = prezzi_basi[1]
-    elif scelta_base == 2 :
-        prezzo_client = prezzi_basi[2]
 
-    # decisione_base= scelta_base
-    tot_base = prezzo_client
-  
+    # prezzo base:
+    if scelta_base == "pasta frolla" :
+        prezzo_client1 = prezzi_basi[0] 
+    elif scelta_base == "pan di spagna":
+        prezzo_client1 = prezzi_basi[1]
+    else :
+        prezzo_client1 = prezzi_basi[2]
+
+    print("Hai scelto :", scelta_base, "prezzo: €",prezzo_client1)
+    
     print ("Ecco gli ingredienti per la farcitura della torta: \n", farciture)
     scelta_farc = input("Scegli un ingrediente per farcirla? \n")
      # prezzo farciture:                   
-    if scelta_farc == 0 :
-        prezzo_client = prezzi_farc[0]
-    elif scelta_farc == 1 :
-        prezzo_client = prezzi_farc[1]
-    elif scelta_farc == 2 :
-        prezzo_client = prezzi_farc[2]
-    
-    # decisione_farc = scelta_farc
-    tot_farc = prezzo_client
+    if scelta_farc == "crema" :
+        prezzo_client2 = prezzi_farc[0]
+    elif scelta_farc == "panna" :
+        prezzo_client2 = prezzi_farc[1]
+    elif scelta_farc == "cioccolata" :
+        prezzo_client2 = prezzi_farc[2]
+
+    print("Hai scelto :", scelta_farc, "prezzo: €",prezzo_client2)
                    
     print ("Ecco la lista dei toppings disponibili : \n", toppings)
     scelta_top = input("Scegli un ingrediente : \n")
     # prezzo toppings:                   
-    if scelta_top == 0 :
-        prezzo_client = prezzi_toppings[0] 
-    elif scelta_top == 1 :
-        prezzo_client = prezzi_toppings[1]
-    elif scelta_base == 2 :
-        prezzo_client = prezzi_toppings[2]
+    if scelta_top == "canditi" :
+        prezzo_client3 = prezzi_top[0] 
+    elif scelta_top == "zucchero a velo" :
+        prezzo_client3 = prezzi_top[1]
+    else :
+        prezzo_client3 = prezzi_top[2]
 
-    # decisione_top = scelta_top
-    tot_top = prezzo_client
+    print("Hai scelto :", scelta_top, "prezzo: €",prezzo_client3)
 
-    tot_torta = tot_base + tot_farc + tot_top                 
+    tot_torta = prezzo_client1 + prezzo_client2 + prezzo_client3
+    print("Lua torta è pronta costerà : €" , tot_torta)
 
-#controllo tavoli disponibili
+    tot_torte += 1
+    ricavi = tot_torta
+    print("Torte vendute :",tot_torte)
+    print("incasso: €", ricavi)                
+ 
     if tavolo == 20 :
         print("Ristorante al completo. Attendi che si liberi un tavolo")
     break
 
-
-
-
-# # Stampa riepilogo dell'ordine
-# print("\n -- Riepilog ordine --")
-# print("Cliente" : nome)
-# print("tavolo": tavolo)
-# print("base": basi[scelta_base] "- €" prezzi_basi[scelta_base])
-# print("farcitura": farciture[scelta_farc] "- €" prezzi_farc[scelta_farc])
-# print("topping":toppings[scelta_top] "- €" prezzi_top[scelta_top])
-
-# # cacolo i ricavi del ristorante = ricavi + tot_cliente
-# # torte vendute tot_torte = tot_torte + count
 
                       
