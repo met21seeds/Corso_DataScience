f556fe6e59a1b88afe02c083eb8a8a55e8dc55c6
Mattia Simini <166040300+met21seeds@users.noreply.github.com>
2024-04-08 12:15:23 +0200

    Update distanza_da_punto.py

--- distanza_da_punto.py
+++ distanza_da_punto.py
@@ -2,9 +2,6 @@ class Punto:
     x = 0
     y = 0
 
-    # def stampa_coordinate(self):
-    #     print("Le coordinate sono", x, y)
-
     def muovi(self, dx, dy):
         self.x += dx
         self.y += dy
@@ -16,10 +13,7 @@ p = Punto()
 dx = int(input("Dammi una nuova coordinata x :\n"))
 dy = int(input("Dammi una nuova coordinata y :\n"))
 
-p.muovi(dx, dy)  # Muovi le coordinate
-#p.stampa_coordinate()  # Stampa le nuove coordinate
+p.muovi()  # Muovi le coordinate
 
-distanza = p.distanza_da_origine()  # Calcola la distanza dall'origine
-print("La distanza dall'origine è", distanza)
-
-
+p.distanza_da_origine()  # Calcola la distanza dall'origine
+print("La distanza dall'origine è", p.distanza_da_origine)
