391e62e5f9f963451d65409ea04f6024cf8bd306
Mattia Simini <166040300+met21seeds@users.noreply.github.com>
2024-04-08 16:08:14 +0200

    Update FunctionRisto.py

--- FunctionRisto.py
+++ FunctionRisto.py
@@ -1,49 +1,53 @@
 # definizione della classe
 class Ristorante:
     def __init__(self, nome, tipo_cucina):
-        self._nome = nome
-        self._tipo_cucina = tipo_cucina
-        self._aperto = False
-        self._menu = {}
-        self._descrizione_cucina = None
+        self.nome = nome
+        self.tipo_cucina = tipo_cucina
+        self.aperto = False
+        self.menu = {
+            "pizza" : 10,
+            "pasta" : 12,
+            "carne" : 20
+        }
+        self.descrizione_cucina = None
 
     # metodi della classe
     # descrivi ristorante
     def descrivi_ristorante(self):
-        if self._descrizione_cucina:
-            print(f"Il ristorante {self._nome} offre {self._tipo_cucina}. Descrizione della cucina: {self._descrizione_cucina}")
+        if self.descrizione_cucina:
+            print(f"Il ristorante {self.nome} offre {self.tipo_cucina}. Descrizione della cucina: {self.descrizione_cucina}")
         else:
-            print(f"Il ristorante {self._nome} offre {self._tipo_cucina}.")
+            print(f"Il ristorante {self.nome} offre {self.tipo_cucina}.")
 
     # apertura risto --> è aperto o chiuso??
     def stato_apertura(self):
-        if self._aperto:
+        if self.aperto:
             print("Il ristorante è aperto.")
         else:
             print("Il ristorante è chiuso.")
 
     # ristorante aperto
     def apri_ristorante(self):
-        self._aperto = True
-        print(f"{self._nome} è ora aperto.")
+        self.aperto = True
+        print(f"{self.nome} è ora aperto.")
 
     # risotante chiuso aperto = False
     def chiudi_ristorante(self):
-        self._aperto = False
-        print(f"{self._nome} è ora chiuso.")
+        self.aperto = False
+        print(f"{self.nome} è ora chiuso.")
 
     #aggiungi un piatto al menu
     def aggiungi_al_menu(self, piatto, prezzo):
-        self._menu[piatto] = prezzo
+        self.menu[piatto] = prezzo
 
     #togli un piatto dal menù
     def togli_dal_menu(self, piatto):
-        if piatto in self._menu:
-            del self._menu[piatto]
+        if piatto in self.menu:
+            del self.menu[piatto]
 
     # stampa menù
     def stampa_menu(self):
-        for piatto, prezzo in self._menu.items():
+        for piatto, prezzo in self.menu.items():
             print(f"{piatto}: {prezzo}€")
 
 
@@ -55,4 +59,3 @@ ristorante.descrivi_ristorante()
 ristorante.stato_apertura()
 ristorante.apri_ristorante()
 ristorante.chiudi_ristorante()
-
