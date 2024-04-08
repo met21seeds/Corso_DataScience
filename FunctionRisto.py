906e424f7aeb64ebce953c02d0f338977ff2e311
Mattia Simini <166040300+met21seeds@users.noreply.github.com>
2024-04-08 15:52:31 +0200

    Update FunctionRisto.py

--- FunctionRisto.py
+++ FunctionRisto.py
@@ -1,61 +1,54 @@
 # definizione della classe
 class Ristorante:
     def __init__(self, nome, tipo_cucina):
-        self.nome = nome
-        self.tipo_cucina = tipo_cucina
-        self.aperto = False
-        self.menu = {}
-        self.descrizione_cucina = None
+        self._nome = nome
+        self._tipo_cucina = tipo_cucina
+        self._aperto = False
+        self._menu = {}
+        self._descrizione_cucina = None
 
-# metodi della classe
-# descrivi ristorante
-def descrivi_ristorante(self):
-    if self.descrizione_cucina:
-        print(f"Il ristorante {self.nome} offre {self.tipo_cucina}. Descrizione della cucina: {self.descrizione_cucina}")
-    else:
-        print(f"Il ristorante {self.nome} offre {self.tipo_cucina}.")
+    # metodi della classe
+    # descrivi ristorante
+    def descrivi_ristorante(self):
+        if self._descrizione_cucina:
+            print(f"Il ristorante {self._nome} offre {self._tipo_cucina}. Descrizione della cucina: {self._descrizione_cucina}")
+        else:
+            print(f"Il ristorante {self._nome} offre {self._tipo_cucina}.")
 
-# def chiedi_descrizione_cucina(self):
-#     risposta = input("Vuoi aggiungere una descrizione della tua cucina? (s/n) ")
-#     if risposta.lower() == 's':
-#         self.descrizione_cucina = input("Inserisci la descrizione della tua cucina: ")
+    # apertura risto --> è aperto o chiuso??
+    def stato_apertura(self):
+        if self._aperto:
+            print("Il ristorante è aperto.")
+        else:
+            print("Il ristorante è chiuso.")
 
-# apertura risto --> è aperto o chiuso??
-def stato_apertura(self):
-    if self.aperto:
-        print("Il ristorante è aperto.")
-    else:
-        print("Il ristorante è chiuso.")
+    # ristorante aperto
+    def apri_ristorante(self):
+        self._aperto = True
+        print(f"{self._nome} è ora aperto.")
 
-# ristorante aperto
-def apri_ristorante(self):
-    self.aperto = True
-    print(f"{self.nome} è ora aperto.")
+    # risotante chiuso aperto = False
+    def chiudi_ristorante(self):
+        self._aperto = False
+        print(f"{self._nome} è ora chiuso.")
 
-# risotante chiuso aperto = False
-def chiudi_ristorante(self):
-    self.aperto = False
-    print(f"{self.nome} è ora chiuso.")
+    #aggiungi un piatto al menu
+    def aggiungi_al_menu(self, piatto, prezzo):
+        self._menu[piatto] = prezzo
+
+    #togli un piatto dal menù
+    def togli_dal_menu(self, piatto):
+        if piatto in self._menu:
+            del self._menu[piatto]
+
+    # stampa menù
+    def stampa_menu(self):
+        for piatto, prezzo in self._menu.items():
+            print(f"{piatto}: {prezzo}€")
+
 
 #Creazione di un'istanza della classe Ristorante
 ristorante = Ristorante("Da Mario", "Italiana")
-
-
-#aggiungi un piatto al menu
-def aggiungi_al_menu(self, piatto, prezzo):
-    self.menu[piatto] = prezzo
-
-#togli un piatto dal menù
-#def togli_dal_menu(self, piatto):
- #       if piatto in self.menu:
-  #          del self.menu[piatto]
-
-# stampa menù
-def stampa_menu(self):
-    for piatto, prezzo in self.menu.items():
-        print(f"{piatto}: {prezzo}€")
-
-
 
 # testare i metodi se funzionano o meno
 ristorante.descrivi_ristorante()
