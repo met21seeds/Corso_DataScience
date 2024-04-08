82658a8d8c0ff18bb307dce0d8c33dfecca5bb74
Mattia Simini <166040300+met21seeds@users.noreply.github.com>
2024-04-08 16:35:33 +0200

    Update FunctionRisto.py

--- FunctionRisto.py
+++ FunctionRisto.py
@@ -3,25 +3,22 @@ class Ristorante:
     def __init__(self, nome, tipo_cucina):
         self.nome = nome
         self.tipo_cucina = tipo_cucina
-        self.aperto = False
+        self.aperto = True
         self.menu = {
-            "pizza" : 10,
-            "pasta" : 12,
-            "carne" : 20
+            "piatti" : {"pizza margherita","pasta","uovo sodo"},
+            "prezzo" : {"12","10","8"}
         }
-        self.descrizione_cucina = None
 
     # metodi della classe
+
     # descrivi ristorante
     def descrivi_ristorante(self):
-        if self.descrizione_cucina:
-            print(f"Il ristorante {self.nome} offre {self.tipo_cucina}. Descrizione della cucina: {self.descrizione_cucina}")
-        else:
-            print(f"Il ristorante {self.nome} offre {self.tipo_cucina}.")
-
+        return print(f"Il ristorante {self.nome} offre {self.tipo_cucina}")
+        
+        
     # apertura risto --> è aperto o chiuso??
     def stato_apertura(self):
-        if self.aperto:
+        if self.aperto == True :
             print("Il ristorante è aperto.")
         else:
             print("Il ristorante è chiuso.")
@@ -38,6 +35,9 @@ class Ristorante:
 
     #aggiungi un piatto al menu
     def aggiungi_al_menu(self, piatto, prezzo):
+        aggiunta = input("Vuoi aggiungere un piatto al menù? \n sì \n no ")
+        if aggiunta == "sì":
+            piatto = input("inserisci")
         self.menu[piatto] = prezzo
 
     #togli un piatto dal menù
@@ -50,6 +50,9 @@ class Ristorante:
         for piatto, prezzo in self.menu.items():
             print(f"{piatto}: {prezzo}€")
 
+# Chiediamo all'utente di inserire i dettagli del ristorante
+nome = input("Inserisci il nome del ristorante: ")
+tipo_cucina = input("Inserisci il tipo di cucina: ")
 
 #Creazione di un'istanza della classe Ristorante
 ristorante = Ristorante("Da Mario", "Italiana")
@@ -59,3 +62,5 @@ ristorante.descrivi_ristorante()
 ristorante.stato_apertura()
 ristorante.apri_ristorante()
 ristorante.chiudi_ristorante()
+ristorante.aggiungi_al_menu()
+ristorante.stampa_menu()
